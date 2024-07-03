362217f053fc69c19aa5733c6e4750833ade4679
Ukinebo1 <perfectprecious2022@gmail.com>
2024-07-03 10:45:05 +0100

    Created a responsive personal website using HTML CSS and JavaScript.

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-let menuIcon = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
-let sections =document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
-let timeDisplay = document.querySelector('#time')
-let dayOfTheWeek = document.querySelector('#day')
-let date = new Date()
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+let timeDisplay = document.querySelector("#time");
+let dayOfTheWeek = document.querySelector("#day");
+let date = new Date();
 
+let hour = date.getHours();
+let minute = date.getMinutes();
+let seconds = date.getSeconds();
+let time = `${hour}:${minute}:${seconds}`;
+timeDisplay.innerHTML = "Current Time: " + time;
 
-let hour = date.getHours()
-let minute =date.getMinutes()
-let seconds = date.getSeconds()
-let time = `${hour}:${minute}:${seconds}`
-timeDisplay.innerHTML= "Current Time: " + time
+let weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = weekday[date.getDay()];
+dayOfTheWeek.innerHTML = `${day}`;
 
-let day = date.getDay()
-dayOfTheWeek.innerHTML = `${day}`
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-window.onscroll = ()=>{
-    sections.forEach(section =>{
-        let top = window.scrollY
-        let offset = section.offsetTop - 150
-        let height = section.offsetHeight
-        let id = section.getAttribute('id')
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + " ]")
+          .classList.add("active");
+      });
+    }
+  });
+};
 
-        if (top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active')
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
-            })
-        }
-    })
-}
-
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
